4de146b2d1f04c16d9c228773209d4133fa2b726
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-03-04 17:14:44 +0900

    Adopt kontainer instead of self hosting stack implementation

--- source/dsh/shellScript.d
+++ source/dsh/shellScript.d
@@ -1,11 +1,13 @@
 module dsh.shellScript;
 import dsh.executeMachine,
-       dsh.environment,
-       dsh.stack;
+       dsh.environment;
+import kontainer.stack;
 import std.algorithm.iteration,
        std.string,
-       std.regex,
        std.array,
+       std.range,
+       std.regex,
+       std.stdio,
        std.conv;
 
 struct EngineProcess {
@@ -41,44 +43,44 @@ class DSHshellScript {
     foreach (k, v; TokenPairs) {
       TokenPairsReversed[v] = k;
     }
-
   }
 
   public bool tokenValidator(string code) {
     Stack!char TokenStack;
     Stack!char QuoteStack;
+    bool inQuote;
 
-    TokenStack.init;
-    TokenStack.autoExtend = false;
-
-    QuoteStack.init;
-    QuoteStack.autoExtend = false;
-
-    foreach (c; code.to!(char[])) {
-      foreach (q; Quotes) {
-        if (c == q) {
-          QuoteStack.push(q);
-          continue;
-        }
-      }
-
-      foreach (b, e; TokenPairs) {
-        if (c == b) {
-          TokenStack.push(c);
-          continue;
+    foreach (m, line; code.split("\n")) {
+      foreach (n, c; line.to!(char[])) {
+        foreach (q; Quotes) {
+          if (c == q) {
+            inQuote = !inQuote;
+            QuoteStack.push(q);
+            break;
+          }
         }
 
-        if (c == e) {
-          if (TokenStack.empty) {
-            return false;
+        if (!inQuote) {
+          if (c in TokenPairs) {
+            TokenStack.push(c);
+            continue;
           }
 
-          char t = TokenStack.pop;
-          if (TokenPairs[t] != c) {
-            return false;
-          }
+          if (c in TokenPairsReversed) {
+            if (TokenStack.empty) {
+              writeln("Invalid - at line:", ++m, " column:", ++n);
+              return false;
+            }
 
-          continue;
+            char t = TokenStack.pop;
+            if (TokenPairs[t] != c) {
+              writeln("Invalid - at line:", ++m, " column:", ++n);
+              writeln("Expected : ", TokenPairs[t], " but given : ", c);
+              return false;
+            }
+
+            continue;
+          }
         }
       }
     }
@@ -88,15 +90,8 @@ class DSHshellScript {
     if (QuoteStack.length % 2 == 0) {
       Stack!char quoteTmp;
 
-      quoteTmp.init;
-      quoteTmp.autoExtend = false;
-
-      import std.range;
       foreach (_; ((QuoteStack.length/2).iota)) {
         quoteTmp.push(QuoteStack.pop);
-      }
-
-      foreach (_; ((QuoteStack.length/2).iota)) {
         if (quoteTmp.pop != QuoteStack.pop) {
           break;
         }
@@ -105,7 +100,7 @@ class DSHshellScript {
       QuoteValid = true;
     }
 
-    return (TokenStack.empty && QuoteValid);
+    return TokenStack.empty && QuoteValid;
   }
 }
 
